7f404586fec5cd030dc304cf78752c23eff16b65
Glaucielle Sá <glauciellesa@gmail.com>
2023-06-10 12:02:58 +0200

    code componentization

--- collection-project/assets/main.js
+++ collection-project/assets/main.js
@@ -17,40 +17,52 @@ const creatStar = (parent, starNumber) => {
   }
 };
 
+const createImage = (parent, image) => {
+  const imgMovie = createNode("div", "movie-img");
+  parent.appendChild(imgMovie);
+  const img = createNode("img", "");
+  img.src = `assets/img/${image}`;
+  imgMovie.appendChild(img);
+};
+
+const createMovieTags = (parent, movie) => {
+  const movieTag = createNode("div", "movie-tag");
+  const classification = createNode("p", "tag-classification");
+  classification.innerHTML = movie.classification;
+  movieTag.appendChild(classification);
+  const movieDuration = createNode("p", "tag-duration");
+  movieDuration.innerHTML = movie.duration;
+  movieTag.appendChild(movieDuration);
+  const movieGender = createNode("p", "tag-gender");
+  movieGender.innerHTML = movie.gender;
+  movieTag.appendChild(movieGender);
+  const movieYear = createNode("p", "tag-year");
+  movieYear.innerHTML = movie.year;
+  movieTag.appendChild(movieYear);
+  parent.appendChild(movieTag);
+};
+
+const createMovieInfo = (parent, movie) => {
+  const movieInformation = createNode("div", "movie-information");
+  parent.appendChild(movieInformation);
+  const titleMovie = createNode("p", "movie-title");
+  titleMovie.innerHTML = movie.title;
+  movieInformation.appendChild(titleMovie);
+  createMovieTags(movieInformation, movie);
+  const movieOverview = createNode("p", "tag-year");
+  movieOverview.innerHTML = movie.overview;
+  movieInformation.appendChild(movieOverview);
+  const movieStar = createNode("div", "movie-star");
+  movieInformation.appendChild(movieStar);
+  creatStar(movieStar, movie.movieStar);
+};
+
 addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("container");
   data.forEach((movie) => {
     const cardMovie = createNode("div", "card-movie");
     container.appendChild(cardMovie);
-    const imgMovie = createNode("div", "movie-img");
-    cardMovie.appendChild(imgMovie);
-    const img = createNode("img", "");
-    img.src = `assets/img/${movie.img}`;
-    imgMovie.appendChild(img);
-    const movieInformation = createNode("div", "movie-information");
-    cardMovie.appendChild(movieInformation);
-    const titleMovie = createNode("p", "movie-title");
-    titleMovie.innerHTML = movie.title;
-    movieInformation.appendChild(titleMovie);
-    const movieTag = createNode("div", "movie-tag");
-    const classification = createNode("p", "tag-classification");
-    classification.innerHTML = movie.classification;
-    movieTag.appendChild(classification);
-    const movieDuration = createNode("p", "tag-duration");
-    movieDuration.innerHTML = movie.duration;
-    movieTag.appendChild(movieDuration);
-    const movieGender = createNode("p", "tag-gender");
-    movieGender.innerHTML = movie.gender;
-    movieTag.appendChild(movieGender);
-    const movieYear = createNode("p", "tag-year");
-    movieYear.innerHTML = movie.year;
-    movieTag.appendChild(movieYear);
-    movieInformation.appendChild(movieTag);
-    const movieOverview = createNode("p", "tag-year");
-    movieOverview.innerHTML = movie.overview;
-    movieInformation.appendChild(movieOverview);
-    const movieStar = createNode("div", "movie-star");
-    movieInformation.appendChild(movieStar);
-    creatStar(movieStar, movie.movieStar);
+    createImage(cardMovie, movie.img);
+    createMovieInfo(cardMovie, movie);
   });
 });
