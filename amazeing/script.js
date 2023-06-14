ede7ec4f75ad3b40ccd1278294d86c8d72c81fa7
Glaucielle Sá <glauciellesa@gmail.com>
2023-06-14 10:51:17 +0200

    fix player size when keypress

--- amazeing/script.js
+++ amazeing/script.js
@@ -199,6 +199,8 @@ addEventListener("DOMContentLoaded", () => {
     ["*", "T", ".", "*", "*", "*", "*", "*"],
     ["*", "*", "*", "*", "*", "*", "*", "*"],
   ];
+  /* 
+  a = l[1][4]; */
 
   const main = document.querySelector("main");
   const createMaze = (level) => {
@@ -207,53 +209,71 @@ addEventListener("DOMContentLoaded", () => {
       const divContent = document.createElement("div");
       divContent.classList = "content";
       main.append(divContent);
-      element.forEach((childrenElem) => {
+      for (let i = 0; i < element.length; i++) {
         const children = document.createElement("div");
         children.classList = "";
         divContent.append(children);
-        if (childrenElem === "*") {
+        if (element[i] === "*") {
           children.classList = "childrenContent walls";
-        } else if (childrenElem === ".") {
+        } else if (element[i] === "." || element[i] === "S") {
           children.classList = "childrenContent paths";
-        } else if (childrenElem === "S") {
-          children.classList = "childrenContent player";
-        } else if (childrenElem === "T") {
+        } else if (element[i] === "T") {
           children.classList = "childrenContent treasure";
         }
-      });
+        if (element[i] === "S") {
+          const player = document.createElement("div");
+          divContent.append(player);
+          player.classList = "childrenContent player";
+          player.setAttribute("id", "player");
+          let sizeSquare = 20;
+          let paddingMymain = 5;
+          let gapMymain = 2;
+          let gapContent = 3.5;
+          if (i < 2) {
+            player.style.left = `${
+              (sizeSquare + paddingMymain + gapMymain) * i
+            }px`;
+            player.style.top = `${sizeSquare + paddingMymain + gapMymain}px`;
+          } else {
+            player.style.left = `${(sizeSquare + gapContent) * i}px`;
+            player.style.top = `${(sizeSquare + gapContent) * i}px`;
+          }
+        }
+      }
     });
   };
 
-  let upDown = 20;
-  let rightLeft = 20;
-
   const keyPress = (e) => {
-    console.log(e.key);
     const divPlayer = document.getElementsByClassName("player")[0];
-
+    const playerSize = 22;
     if (e.key === "ArrowUp") {
       console.log("up");
       divPlayer.style.top = upDown + "px";
-      upDown -= 22;
+      upDown -= playerSize;
       console.log(upDown);
     } else if (e.key === "ArrowDown") {
       console.log("down");
       divPlayer.style.top = upDown + "px";
-      upDown += 22;
+      upDown += playerSize;
       console.log(upDown);
     } else if (e.key == "ArrowRight") {
       console.log("Right");
       divPlayer.style.left = rightLeft + "px";
-      rightLeft += 22;
+      rightLeft += playerSize;
       console.log(rightLeft);
     } else if (e.key == "ArrowLeft") {
       console.log("Left");
       divPlayer.style.left = rightLeft + "px";
-      rightLeft -= 22;
+      rightLeft -= playerSize;
       console.log(rightLeft);
     }
   };
 
-  createMaze(LEVEL_1);
   document.addEventListener("keydown", keyPress);
+  createMaze(LEVEL_1);
+  const divPlayer = document.getElementById("player");
+  const left = divPlayer.style.left.replace("px", ""); //'70.5px'
+  const top = divPlayer.style.top.replace("px", ""); //'70.5px'
+  let upDown = Number(top);
+  let rightLeft = Number(left);
 });
