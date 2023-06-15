ad825383ae59d3ce087a6a44b1f2dcc465a2f127
Glaucielle Sá <glauciellesa@gmail.com>
2023-06-15 13:45:22 +0200

    Add toggle menu to css

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const divContent = document.querySelector("#content");
 const divProjects = document.querySelector("#projects");
 const divNavbar = document.querySelector("#navbar");
 const divShowMenu = document.querySelector("#showMenu");
@@ -8,11 +9,12 @@ const toogleMenu = (e) => {
   if (divNavbar.classList.contains("hideNavbar")) {
     divNavbar.classList = "navbar showNavbar";
     divShowMenu.classList = "menu hideNavbar";
-    divCloseMenu.classList = "close-menu ";
+    divCloseMenu.classList = "close-menu showNavbar";
+    divContent.style.display = "inline-block";
   } else {
     divNavbar.classList = "navbar hideNavbar";
-    divShowMenu.classList = "menu hideNavbar";
-    divCloseMenu.classList = "close-menu showNavbar";
+    divShowMenu.classList = "menu showNavbar";
+    divCloseMenu.classList = "close-menu hideNavbar";
   }
   console.log(divNavbar.classList);
 };
