ca1d53ae0b460656bb70eb8b03630a7503de839d
Glaucielle Sá <glauciellesa@gmail.com>
2023-06-30 12:03:48 +0200

    fix css

--- weather-app/script.js
+++ weather-app/script.js
@@ -37,13 +37,14 @@ const displayWeatherCard = (weatherWeek) => {
         createTextNode(element.weekDay),
       ]),
       createElement("img", {
+        src: "imgIcon",
         src: `http://openweathermap.org/img/w/${element.icon}.png`,
       }),
       createElement("div", { class: "max-weather" }, [
-        createTextNode(`Max: ${Math.floor(element.maxTemperature)}˚`),
+        createTextNode(`Max: ${Math.round(element.maxTemperature)}˚`),
       ]),
       createElement("div", { class: "min-weather" }, [
-        createTextNode(`Min: ${Math.floor(element.minTemperature)}˚`),
+        createTextNode(`Min: ${Math.round(element.minTemperature)}˚`),
       ]),
       createElement("div", { class: "description-weather" }, [
         createTextNode(element.weatherInfo),
@@ -137,21 +138,67 @@ const displayCurrentWeather = (data) => {
 const getWeatherData = (location) => {
   const apiKey = "b73209fdcdbea6b6ada4ed11b21e9494";
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${apiKey}`;
-
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      console.log({ data });
       reduceDataWeather(data);
       displayCurrentWeather(data);
     });
 };
 
+const displayImage = (imgLink) => {
+  /*   divWeather = document.querySelector(".img-city");
+   */ divWeather = document.querySelector(".current-weather");
+  divWeather.style.backgroundImage = `url("${imgLink}")`;
+  divWeather.style.backgroundPosition = "center";
+  divWeather.style.backgroundSize = "cover";
+  divWeather.style.backgroundRepeat = "no-repeat";
+
+  /*  let cityImg = createElement("img", {
+    class: "city-img",
+    src: imgLink,
+  });
+  divWeather.innerHTML = "";
+  divWeather.append(cityImg); */
+};
+
+const runImgArray = (data) => {
+  console.log(data);
+  let i = 0;
+  displayImage(data[i].urls.regular);
+  i++;
+  setInterval(() => {
+    if (i < data.length) {
+      console.log(data[i].urls.regular);
+      displayImage(data[i].urls.regular);
+    } else {
+      i = 0;
+    }
+    i++;
+  }, 1000);
+};
+
+const getImageData = (location) => {
+  const apiKey = "s7a3o6Wi4qd5MlZ5tlSEqqVN1Vx91LrcDevXZ0GuJCE";
+  const url = `https://api.unsplash.com/search/photos?query=${location}&client_id=${apiKey}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      runImgArray(data.results);
+    });
+};
+
 const cityInput = document.querySelector("#city_input");
 const searchInfo = document.querySelector(".search-info");
+const container = document.querySelector(".container");
+container.style.display = "none";
 document.querySelector("button").addEventListener("click", () => {
   console.log("btn clicked");
   searchInfo.style.display = "none";
+  container.style.display = "inline-block";
   getWeatherData(cityInput.value);
+  getImageData(cityInput.value);
 });
 
 /* Add keydown event listener to document rather than button */
